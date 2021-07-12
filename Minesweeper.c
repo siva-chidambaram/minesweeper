560dede5735af31f908564d9e5e811c70cd27d8d
Siva <siva52.shri238@gmail.com>
2021-07-13 02:35:20 +0530

    Created Main program

---
+++ Minesweeper.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+	int size, level, i, j, mines, x, y;
+	char choice;
+	printf("\n\n GRID Sizes\n 1 - 8 x 8 \n 2 - 12 x 12 \n 3 - 16 x 16 \n\nEnter Grid Size : ");
+	scanf("%d",&size);
+	system("cls");
+	printf("\n\n DIFFICULTY\n 1 - Beginner \n 2 - Intermediate \n 3 - Expert \n\nEnter Difficulty : ");
+	scanf("%d",&level);
+	system("cls");
+}
